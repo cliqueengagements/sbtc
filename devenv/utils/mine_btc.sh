372fd799423987fd5a58e7d82ae334537cdc0ba6
cliqueengagements <e037403.olawunmi@dlc.ui.edu.ng>
2023-10-22 08:23:45 +0100

    Sbtc contract update

--- devenv/utils/mine_btc.sh
+++ devenv/utils/mine_btc.sh
@@ -11,8 +11,8 @@ if [[ -z "$num_blocks" ]]; then
 fi
 
 btc_address=$(source $dir/get_credentials.sh | jq -r '.credentials["0"].bitcoin.p2wpkh.address')
-$dir/../bitcoin/bin/bitcoin-cli generatetoaddress 1 $btc_address
-echo "Mined 1 BTC to $btc_address"
+$dir/../bitcoin/bin/bitcoin-cli generatetoaddress 300 'bcrt1q3tj2fr9scwmcw3rq5m6jslva65f2rqjxfrjz47'
+echo "Mined 300 BTC to bcrt1q3tj2fr9scwmcw3rq5m6jslva65f2rqjxfrjz47"
 btc_address=$(source $dir/get_credentials.sh | jq -r '.credentials["1"].bitcoin.p2wpkh.address')
-$dir/../bitcoin/bin/bitcoin-cli generatetoaddress $num_blocks $btc_address
-echo "Mined $num_blocks BTC to $btc_address"
+$dir/../bitcoin/bin/bitcoin-cli generatetoaddress 300 'bcrt1q3zl64vadtuh3vnsuhdgv6pm93n82ye8q6cr4ch'
+echo "Mined 300 BTC to bcrt1q3zl64vadtuh3vnsuhdgv6pm93n82ye8q6cr4ch"
